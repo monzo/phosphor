6bb10295bda8a63fc0c48c73b5dc024ddd5faf55
Matt Heath <matt@mattheath.com>
2015-02-04 18:00:11 +0000

    Simplify domain

--- domain/domain.go
+++ domain/domain.go
@@ -4,10 +4,21 @@ import "time"
 
 // Trace represents a full trace of a request
 // comprised of a number of events and annotations
-type Trace struct {
-	Events      []Event
-	Annotations []Annotation
-}
+type Trace []Event
+
+// EventType represents an Enum of types of Events which Phosphor can record
+type EventType int
+
+const (
+	// RPC Calls
+	Req = EventType(1) // Client Request dispatch
+	Rsp = EventType(2) // Client Response received
+	In  = EventType(3) // Server Request received
+	Out = EventType(4) // Server Response dispatched
+
+	// Developer initiated annotations
+	Annotation = EventType(5)
+)
 
 // An Event represents a section of an RPC call between systems
 type Event struct {
@@ -22,21 +33,9 @@ type Event struct {
 	Origin      string // Fully qualified name of the message origin
 	Destination string // Fully qualified name of the message destination
 
-	EventType   string // The type of Event
-	Payload     string // The returned payload (if possible)
-	PayloadSize int32  // Bytes of payload
-}
+	EventType EventType // The type of Event
 
-// Annotation describes an optional additional annotation which occurs within a call
-type Annotation struct {
-	TraceId       string
-	EventId       string
-	ParentEventId string
-
-	Timestamp time.Time // Timestamp the event occured, can only be compared on the same machine
-
-	Hostname string // Hostname this event originated from
-
-	Content  string            // Arbitrary content, eg. debug output
-	KeyValue map[string]string // Key value debug information
+	Payload     string            // The payload, eg. RPC body, or Annotation
+	PayloadSize int32             // Bytes of payload
+	KeyValue    map[string]string // Key value debug information
 }
