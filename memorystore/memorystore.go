14fb18f922c44dcfa0a27b56a54b0014b59423c4
Matt Heath <matt@mattheath.com>
2015-02-05 11:11:07 +0000

    Fix up memstore

--- memorystore/memorystore.go
+++ memorystore/memorystore.go
@@ -8,31 +8,31 @@ import (
 
 func New() *MemoryStore {
 	return &MemoryStore{
-		store: make(map[string]Trace),
+		store: make(map[string]domain.Trace),
 	}
 }
 
 type MemoryStore struct {
 	sync.RWMutex
-	store map[string]Trace
+	store map[string]domain.Trace
 }
 
-func (s *MemoryStore) GetTrace(id string) (Trace, error) {
+func (s *MemoryStore) GetTrace(id string) (domain.Trace, error) {
 	s.RLock()
 	defer s.RUnlock()
 
 	return s.store[id], nil
 }
 
-func (s *MemoryStore) StoreTraceEvent(e domain.Event) error {
+func (s *MemoryStore) StoreTraceFrame(f domain.Frame) error {
 	s.Lock()
 	defer s.Unlock()
 
 	// Load our current trace
 	t := s.store[e.TraceId]
 
-	// Add the new event to this
-	t = append(t, e)
+	// Add the new frame to this
+	t = append(t, f)
 
 	// Store it back
 	s.store[e.TraceId] = t
