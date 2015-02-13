e4161c7759ec517554e1c8b6d13e1876a7396c4a
Matt Heath <matt@mattheath.com>
2015-02-13 04:01:36 +0000

    Attempt to publish to nsqd nodes until we succeed, or run out of nodes

--- transport/nsq.go
+++ transport/nsq.go
@@ -1,11 +1,16 @@
 package transport
 
 import (
+	"errors"
+	"math/rand"
+
 	nsq "github.com/bitly/go-nsq"
 	log "github.com/cihub/seelog"
 
 	"github.com/mattheath/phosphor/util"
 )
+
+var ErrPublishFailure = errors.New("Failed to publish to NSQD")
 
 // NewNSQTransport initialises a Transport over NSQ
 func NewNSQTransport(topic string, nsqdTCPAddrs util.StringArray) (Transport, error) {
@@ -35,5 +40,25 @@ type NSQPublisher struct {
 }
 
 func (p *NSQPublisher) MultiPublish(body [][]byte) error {
-	return nil
+
+	// Round robin, from a random starting position
+	i := rand.Intn(len(p.producers)) - 1
+
+	// Attempt up to our number of configured nodes
+	for attempt := 0; attempt < len(p.producers); attempt++ {
+		pd := p.producers[i]
+		if err := pd.MultiPublish(p.topic, body); err == nil {
+			// success!
+			return nil
+		}
+
+		// Move to next host, or cycle back around
+		i++
+		if i >= len(p.producers) {
+			i = 0
+		}
+	}
+
+	// We've run out of nodes, and not managed to publish
+	return ErrPublishFailure
 }
