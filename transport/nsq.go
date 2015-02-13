d770ee57ec69610654d17e2723129f685698b828
Matt Heath <matt@mattheath.com>
2015-02-13 03:20:30 +0000

    Add NSQ producer initialisation

--- transport/nsq.go
+++ transport/nsq.go
@@ -1,10 +1,38 @@
 package transport
 
-func NewNSQTransport() Transport {
-	return &NSQPublisher{}
+import (
+	nsq "github.com/bitly/go-nsq"
+	log "github.com/cihub/seelog"
+
+	"github.com/mattheath/phosphor/util"
+)
+
+// NewNSQTransport initialises a Transport over NSQ
+func NewNSQTransport(topic string, nsqdTCPAddrs util.StringArray) (Transport, error) {
+
+	// Currently using default config
+	cfg := nsq.NewConfig()
+
+	// Create a producer for each nsqd node provided
+	producers := make(map[string]*nsq.Producer)
+	for _, addr := range nsqdTCPAddrs {
+		producer, err := nsq.NewProducer(addr, cfg)
+		if err != nil {
+			log.Warnf("failed to create nsq.Producer - %s", err)
+		}
+		producers[addr] = producer
+	}
+
+	return &NSQPublisher{
+		topic:     topic,
+		producers: producers,
+	}, nil
 }
 
-type NSQPublisher struct{}
+type NSQPublisher struct {
+	topic     string
+	producers map[string]*nsq.Producer
+}
 
 func (p *NSQPublisher) MultiPublish(body [][]byte) error {
 	return nil
