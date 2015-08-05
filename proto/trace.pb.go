017ccf7a7bbafe7434d98753b9aa2b1c79053ada
Matt Heath <matt@mattheath.com>
2015-08-05 13:11:41 +0100

    Update proto - s/Frame/Annotation

--- proto/trace.pb.go
+++ proto/trace.pb.go
@@ -9,31 +9,31 @@ It is generated from these files:
 	github.com/mattheath/phosphor/proto/trace.proto
 
 It has these top-level messages:
-	TraceFrame
+	Annotation
 	KeyValue
 */
 package proto
 
-import proto "github.com/golang/protobuf/proto"
+import proto1 "github.com/golang/protobuf/proto"
 import math "math"
 
 // Reference imports to suppress errors if they are not otherwise used.
-var _ = proto.Marshal
+var _ = proto1.Marshal
 var _ = math.Inf
 
-type FrameType int32
+type AnnotationType int32
 
 const (
-	FrameType_UNKNOWN    FrameType = 0
-	FrameType_REQ        FrameType = 1
-	FrameType_REP        FrameType = 2
-	FrameType_IN         FrameType = 3
-	FrameType_OUT        FrameType = 4
-	FrameType_TIMEOUT    FrameType = 5
-	FrameType_ANNOTATION FrameType = 6
+	AnnotationType_UNKNOWN    AnnotationType = 0
+	AnnotationType_REQ        AnnotationType = 1
+	AnnotationType_REP        AnnotationType = 2
+	AnnotationType_IN         AnnotationType = 3
+	AnnotationType_OUT        AnnotationType = 4
+	AnnotationType_TIMEOUT    AnnotationType = 5
+	AnnotationType_ANNOTATION AnnotationType = 6
 )
 
-var FrameType_name = map[int32]string{
+var AnnotationType_name = map[int32]string{
 	0: "UNKNOWN",
 	1: "REQ",
 	2: "REP",
@@ -42,7 +42,7 @@ var FrameType_name = map[int32]string{
 	5: "TIMEOUT",
 	6: "ANNOTATION",
 }
-var FrameType_value = map[string]int32{
+var AnnotationType_value = map[string]int32{
 	"UNKNOWN":    0,
 	"REQ":        1,
 	"REP":        2,
@@ -52,25 +52,25 @@ var FrameType_value = map[string]int32{
 	"ANNOTATION": 6,
 }
 
-func (x FrameType) Enum() *FrameType {
-	p := new(FrameType)
+func (x AnnotationType) Enum() *AnnotationType {
+	p := new(AnnotationType)
 	*p = x
 	return p
 }
-func (x FrameType) String() string {
-	return proto.EnumName(FrameType_name, int32(x))
+func (x AnnotationType) String() string {
+	return proto1.EnumName(AnnotationType_name, int32(x))
 }
-func (x *FrameType) UnmarshalJSON(data []byte) error {
-	value, err := proto.UnmarshalJSONEnum(FrameType_value, data, "FrameType")
+func (x *AnnotationType) UnmarshalJSON(data []byte) error {
+	value, err := proto1.UnmarshalJSONEnum(AnnotationType_value, data, "AnnotationType")
 	if err != nil {
 		return err
 	}
-	*x = FrameType(value)
+	*x = AnnotationType(value)
 	return nil
 }
 
-type TraceFrame struct {
-	// The ID of the trace this frame is a component of
+type Annotation struct {
+	// The ID of the trace this annotation is a component of
 	TraceId *string `protobuf:"bytes,1,req,name=trace_id" json:"trace_id,omitempty"`
 	// The span this trace corresponds to, in the case this
 	// is representing a service (REQ/REP) call
@@ -78,13 +78,9 @@ type TraceFrame struct {
 	// The parent span this trace corresponds to, allowing us
 	// to correlate trace frames and reconstruct the request
 	ParentId *string `protobuf:"bytes,3,opt,name=parent_id" json:"parent_id,omitempty"`
-	// The type of trace frame we're capturing
-	Type *FrameType `protobuf:"varint,4,req,name=type,enum=proto.FrameType" json:"type,omitempty"`
+	// The type of annotation we're capturing
+	Type *AnnotationType `protobuf:"varint,4,req,name=type,enum=proto.AnnotationType" json:"type,omitempty"`
 	// Time since the epoch in nanoseconds
-	//
-	// 64bit time in nanoseconds with the default unix epoch gives us the following range:
-	// Min time: 1677-09-21 00:12:43.145224192 +0000 UTC
-	// Max time: 2262-04-11 23:47:16.854775807 +0000 UTC
 	Timestamp *int64 `protobuf:"varint,5,req,name=timestamp" json:"timestamp,omitempty"`
 	// Duration in nanoseconds
 	// This should only be used to measure time on the same node
@@ -92,9 +88,9 @@ type TraceFrame struct {
 	Duration *int64 `protobuf:"varint,6,opt,name=duration" json:"duration,omitempty"`
 	// Machine hostname, container name etc
 	Hostname *string `protobuf:"bytes,7,opt,name=hostname" json:"hostname,omitempty"`
-	// Origin of this frame, likely a service
+	// Origin of this annotation, likely a service or application for a RPC
 	Origin *string `protobuf:"bytes,8,opt,name=origin" json:"origin,omitempty"`
-	// Destination of this frame's action
+	// Destination of this annotations action
 	// eg. the service which a request was destined for
 	// likely not set for annotations
 	Destination *string `protobuf:"bytes,9,opt,name=destination" json:"destination,omitempty"`
@@ -105,81 +101,81 @@ type TraceFrame struct {
 	XXX_unrecognized []byte      `json:"-"`
 }
 
-func (m *TraceFrame) Reset()         { *m = TraceFrame{} }
-func (m *TraceFrame) String() string { return proto.CompactTextString(m) }
-func (*TraceFrame) ProtoMessage()    {}
+func (m *Annotation) Reset()         { *m = Annotation{} }
+func (m *Annotation) String() string { return proto1.CompactTextString(m) }
+func (*Annotation) ProtoMessage()    {}
 
-func (m *TraceFrame) GetTraceId() string {
+func (m *Annotation) GetTraceId() string {
 	if m != nil && m.TraceId != nil {
 		return *m.TraceId
 	}
 	return ""
 }
 
-func (m *TraceFrame) GetSpanId() string {
+func (m *Annotation) GetSpanId() string {
 	if m != nil && m.SpanId != nil {
 		return *m.SpanId
 	}
 	return ""
 }
 
-func (m *TraceFrame) GetParentId() string {
+func (m *Annotation) GetParentId() string {
 	if m != nil && m.ParentId != nil {
 		return *m.ParentId
 	}
 	return ""
 }
 
-func (m *TraceFrame) GetType() FrameType {
+func (m *Annotation) GetType() AnnotationType {
 	if m != nil && m.Type != nil {
 		return *m.Type
 	}
-	return FrameType_UNKNOWN
+	return AnnotationType_UNKNOWN
 }
 
-func (m *TraceFrame) GetTimestamp() int64 {
+func (m *Annotation) GetTimestamp() int64 {
 	if m != nil && m.Timestamp != nil {
 		return *m.Timestamp
 	}
 	return 0
 }
 
-func (m *TraceFrame) GetDuration() int64 {
+func (m *Annotation) GetDuration() int64 {
 	if m != nil && m.Duration != nil {
 		return *m.Duration
 	}
 	return 0
 }
 
-func (m *TraceFrame) GetHostname() string {
+func (m *Annotation) GetHostname() string {
 	if m != nil && m.Hostname != nil {
 		return *m.Hostname
 	}
 	return ""
 }
 
-func (m *TraceFrame) GetOrigin() string {
+func (m *Annotation) GetOrigin() string {
 	if m != nil && m.Origin != nil {
 		return *m.Origin
 	}
 	return ""
 }
 
-func (m *TraceFrame) GetDestination() string {
+func (m *Annotation) GetDestination() string {
 	if m != nil && m.Destination != nil {
 		return *m.Destination
 	}
 	return ""
 }
 
-func (m *TraceFrame) GetPayload() string {
+func (m *Annotation) GetPayload() string {
 	if m != nil && m.Payload != nil {
 		return *m.Payload
 	}
 	return ""
 }
 
-func (m *TraceFrame) GetKeyValue() []*KeyValue {
+func (m *Annotation) GetKeyValue() []*KeyValue {
 	if m != nil {
 		return m.KeyValue
 	}
@@ -193,7 +189,7 @@ type KeyValue struct {
 }
 
 func (m *KeyValue) Reset()         { *m = KeyValue{} }
-func (m *KeyValue) String() string { return proto.CompactTextString(m) }
+func (m *KeyValue) String() string { return proto1.CompactTextString(m) }
 func (*KeyValue) ProtoMessage()    {}
 
 func (m *KeyValue) GetKey() string {
@@ -211,5 +207,5 @@ func (m *KeyValue) GetValue() string {
 }
 
 func init() {
-	proto.RegisterEnum("proto.FrameType", FrameType_name, FrameType_value)
+	proto1.RegisterEnum("proto.AnnotationType", AnnotationType_name, AnnotationType_value)
 }
